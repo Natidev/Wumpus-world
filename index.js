8eeb280ebc34c2d9c680d6ebf2f0bb3f5b23b2fb
Natidev <53361365+Natidev@users.noreply.github.com>
2025-01-24 02:45:45 +0300

    before changing to string

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const canvas = document.getElementsByTagName('canvas')[0];
 const ctx = canvas.getContext('2d',{willReadFrequently:true});
 const gridSize = 4;
 const cellSize = canvas.width / gridSize;
-
+let stinkSet=new Set()
+let breezeSet=new Set()
 const gold = new Image();
 const wumpus = new Image();
 const stink = new Image();
@@ -33,16 +34,32 @@ for (let y = 0; y <= canvas.height; y += cellSize) {
 
 ctx.strokeStyle = 'black';
 ctx.stroke();
-
-
+const validateMove=()=>{
+    console.log(position,wumpusPos)
+    console.log(position==wumpusPos);
+    
+    if(position.x==wumpusPos.x && position.y==wumpusPos.y)
+        alert("You got Eaten")
+    else if (position.x==wumpusPos.x && position.y==wumpusPos.y)
+        alert("Falled into a pit")
+    else if(stinkSet.has(position))
+        alert("Wumpus is close")
+    else if(breezeSet.has(position))
+        alert("Pit nearby")
+}
+gold.onload=()=>ctx.drawImage(gold,2*cellSize,0*cellSize,cellSize,cellSize)
 dude.onload = function() {
     ctx.drawImage(dude, 0, position.y*cellSize, cellSize, cellSize);}
-function clearPrevious() {
-        ctx.clearRect(position.x * cellSize, position.y * cellSize, cellSize, cellSize);
-    }
-function clearPrevious(x,y) {
+    function clearPreviousA(x,y) {
         ctx.clearRect(x * cellSize, y * cellSize, cellSize, cellSize);
+        drawGrid();
+        putWumpus();
+        putPit();
     }
+    function clearPrevious() {
+            ctx.clearRect(position.x * cellSize, position.y * cellSize, cellSize, cellSize);
+            
+        }
 
 function putWumpus(){
     ctx.drawImage(wumpus, wumpusPos.x * cellSize, wumpusPos.y * cellSize, cellSize, cellSize);
@@ -56,11 +73,30 @@ function putWumpus(){
     if (wumpusPos.y != 0)
         putStink(wumpusPos.x, wumpusPos.y - 1);
 }
+function putPit() {
+    ctx.drawImage(pit, pitPos.x * cellSize, pitPos.y * cellSize, cellSize, cellSize);
 
+    if (pitPos.x < 3)
+        putBreeze(pitPos.x + 1, pitPos.y);
+    if (pitPos.x != 0)
+        putBreeze(pitPos.x - 1, pitPos.y);
+    if (pitPos.y < 3)
+        putBreeze(pitPos.x, pitPos.y + 1);
+    if (pitPos.y != 0)
+        putBreeze(pitPos.x, pitPos.y - 1);
+}
+
+const putBreeze = (x, y) => {
+    breezeSet.add({x:x,y:y})
+    ctx.drawImage(breeze, x * cellSize, y * cellSize, cellSize, cellSize);
+};
+
+pit.onload = putPit;
 wumpus.onload = putWumpus;
 
 
 const putStink=(x,y)=>{
+    stinkSet.add({x:x,y:y})
         ctx.drawImage(stink,x*cellSize,y*cellSize,cellSize,cellSize);
 
 }
@@ -71,6 +107,7 @@ function down(){
         position.y=position.y+1
        
         ctx.drawImage(dude, position.x*cellSize, position.y*cellSize, cellSize, cellSize);
+        validateMove()
     }
 }
 function up(){
@@ -78,6 +115,7 @@ function up(){
         clearPrevious()
         position.y=position.y-1
         ctx.drawImage(dude, position.x*cellSize, position.y*cellSize, cellSize, cellSize)
+        validateMove()
     }
 }
 function right(){
@@ -85,6 +123,7 @@ function right(){
         clearPrevious()
         position.x=position.x+1
         ctx.drawImage(dude, position.x*cellSize, position.y*cellSize, cellSize, cellSize);
+        validateMove()
     }
 }
 function left(){
@@ -92,6 +131,7 @@ function left(){
         clearPrevious()
         position.x=position.x-1
         ctx.drawImage(dude, position.x*cellSize, position.y*cellSize, cellSize, cellSize);
+        validateMove()
     }
 }
 document.addEventListener('keydown', function(event) {
